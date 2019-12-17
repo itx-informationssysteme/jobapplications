c4c8168b0486c58142e9cc3074d0c88c41dc681c
Benjamin Jasper <benjamin.jasper@itx.de>
2019-12-17 15:23:35 +0100

    added optional Bootstrap Static Template

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,3 +1,9 @@
 <?php
 	defined('TYPO3_MODE') || die();
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('jobs', 'Configuration/TypoScript', 'Jobs');
+
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+		'jobs',
+		'Configuration/TypoScript/IncludeBootstrap',
+		'Include Bootstrap'
+	);
