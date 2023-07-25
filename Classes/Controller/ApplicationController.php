b4a0e87cb96f72a8b431cff58b4a244cd21f9f0e
Benjamin Jasper <benjamin.jasper@itx.de>
2023-07-25 16:18:39 +0200

    wip: #99 update TYPO3 12: fixed bug in honeypot

--- Classes/Controller/ApplicationController.php
+++ Classes/Controller/ApplicationController.php
@@ -327,7 +327,7 @@
 
 			if (array_key_exists("honeypot", $this->settings) && $this->settings["honeypot"] === '1')
 			{
-				$this->checkHoneypot($arguments, $posting);
+				$this->checkHoneypot($arguments, $posting, $newApplication);
 			}
 
 			// Normalize file array -> free choice whether multi or single upload
@@ -689,19 +689,20 @@
 			}
 		}
 
-		/**
-		 * @param array   $arguments
-		 * @param Posting $posting
-		 *
-		 * @return void
-		 */
-		private function checkHoneypot(array $arguments, ?Posting $posting): void
+        /**
+         * @param array            $arguments
+         * @param Posting|null     $posting
+         * @param Application|null $newApplication
+         *
+         * @return void
+         */
+		private function checkHoneypot(array $arguments, ?Posting $posting, ?Application $newApplication): void
 		{
 			//Minimal Seconds it should take someone to fill out the form.
 			$minMillisecondsToFillOutForm = 1000;
 
 			//Is the honeypot field not empty or was the form filled out extremely fast. If yes it is likely due to a bot. Do not send form redirect to page and display error.
-			if (($arguments["new_mail"] ?? '') !== '' || ($arguments["timestamp"] != '' && (microtime(as_float: true) - (int)$arguments["timestamp"]) < $minMillisecondsToFillOutForm))
+			if (($newApplication && ($arguments["new_mail"] ?? '') !== '') || ($arguments["timestamp"] != '' && (microtime(as_float: true) - (int)$arguments["timestamp"]) < $minMillisecondsToFillOutForm))
 			{
 				$this->addFlashMessage("That shouldn't have happened, please try again.", "Oops", FlashMessage::ERROR);
 				$this->redirect("new", "Application", null, ["posting" => $posting]);
