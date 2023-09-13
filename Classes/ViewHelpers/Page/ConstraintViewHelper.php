6087e3233ac513da024b66019698e78aaf9140d1
Benjamin Jasper <benjamin.jasper@itx.de>
2023-09-13 10:24:28 +0200

    fix: #90 added dynamic constraint properties detection

--- Classes/ViewHelpers/Page/ConstraintViewHelper.php
+++ Classes/ViewHelpers/Page/ConstraintViewHelper.php
@@ -1,42 +1,72 @@
 <?php
 
-	namespace ITX\Jobapplications\ViewHelpers\Page;
+namespace ITX\Jobapplications\ViewHelpers\Page;
 
-	use ITX\Jobapplications\Domain\Model\Constraint;
-	use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use ITX\Jobapplications\Domain\Model\Constraint;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+/**
+ * This file is part of the ITX\jobapplications project under GPLv2 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * replaces string with substring
+ */
+class ConstraintViewHelper extends AbstractViewHelper
+{
+	use CompileWithRenderStatic;
+
+	protected static array $reflectionCache = [];
+
+	public function initializeArguments()
+	{
+		parent::initializeArguments();
+		$this->registerArgument('page', 'string', 'ViewHelper page', true);
+		$this->registerArgument('constraint', 'mixed', 'ViewHelper constraint', true);
+	}
 
 	/**
-	 * This file is part of the ITX\jobapplications project under GPLv2 or later.
+	 * @param array                     $arguments
+	 * @param \Closure                  $renderChildrenClosure
+	 * @param RenderingContextInterface $renderingContext *
 	 *
-	 * For the full copyright and license information, please read the
-	 * LICENSE.md file that was distributed with this source code.
-	 *
-	 * replaces string with substring
+	 * @return array which contains the page and constraints for the pagination template
 	 */
-
-	class ConstraintViewHelper extends AbstractViewHelper
+	public static function renderStatic(array                     $arguments,
+										\Closure                  $renderChildrenClosure,
+										RenderingContextInterface $renderingContext): array
 	{
-		public function initializeArguments() {
-			parent::initializeArguments();
-			$this->registerArgument('page', 'string', 'ViewHelper page', true);
-			$this->registerArgument('constraint', 'mixed', 'ViewHelper constraint', true);
-		}
+		$page = (string)$arguments["page"];
+		/** @var Constraint $constraint */
+		$constraintArgument = $arguments["constraint"];
 
-		/**
-		 * @return array which contains the page and constraints for the pagination template
-		 */
-		public function render()
-		{
-			$page = $this->arguments["page"];
-			/** @var Constraint $constraint */
-			$constraintArgument = $this->arguments["constraint"];
-
-			return  [
+		$returnArguments = [
 				"page" => $page,
-				"constraint][division][]" => $constraintArgument->getDivision() ?? "",
-				"constraint][careerLevel][]" => $constraintArgument->getCareerLevel() ?? "",
-				"constraint][employmentType][]" => $constraintArgument->getEmploymentType() ?? "",
-				"constraint][locations][]" => $constraintArgument->getLocations() ?? "",
-				];
+		];
+
+		$properties = self::getReflectedProperties($constraintArgument);
+
+		foreach ($properties as $property) {
+			$type = $property->getType()?->getName() ?? 'array';
+
+			$identifier = "constraint][{$property->getName()}]";
+			if ($type === 'array') {
+				$identifier .= "[]";
+			}
+
+			$returnArguments[$identifier] = $property->getValue($constraintArgument);
 		}
+
+		return $returnArguments;
 	}
+
+	protected static function getReflectedProperties(object $constraint): array
+	{
+		$class = get_class($constraint);
+
+		return self::$reflectionCache[$class] ?? (new \ReflectionClass(get_class($constraint)))->getProperties();
+	}
+}
